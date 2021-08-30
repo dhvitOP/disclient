872be399b2f41ddb723056398256389cea7f85a7
tsdon0001 <mchatterjee6291@gmail.com>
2021-08-30 10:56:17 +0530

    disclient v2

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const discord = require('./lib/index');
 
-const client = new discord.Client('ODEzNjQ5NjAzODQ4NDM3Nzcw.YDSYPg.3k2e-wBXs8ESMElE9OwL5U51FtU');
+const client = new discord.Client('TOKEN');
 
 client.on('READY', () => {
   console.log(`${client.user.tag} is online`);
