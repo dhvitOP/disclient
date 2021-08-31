461ad3c413629eecc41481da35479d7733a3359d
tsdon0001 <mchatterjee6291@gmail.com>
2021-08-31 18:13:24 +0530

    Minor changes

---
+++ src/handlers/MESSAGE_UPDATE.ts
@@ -0,0 +1,6 @@
+import { Client } from "../Client/Client";
+import { Payload } from "../interfaces";
+
+export default async function (client: Client, payload: Payload){
+    client.emit(payload.t, payload.d);
+}
